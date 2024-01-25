7d01b93cc73d457bd03f975f38bee0a25442d3f1
Thiago Bs <thiago-fa@hotmail.com>
2024-01-24 21:59:00 -0300

    Chart Bar

--- dssales-frontend/src/App.tsx
+++ dssales-frontend/src/App.tsx
@@ -2,6 +2,7 @@
 import './App.css'
 import Filter from './components/filter'
 import Header from './components/header'
+import SalesByDate from './components/sales-by-date'
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
       <Header />
       <div className='app-container'>
         <Filter/>
+        <SalesByDate />
       </div>
     </>
   )
